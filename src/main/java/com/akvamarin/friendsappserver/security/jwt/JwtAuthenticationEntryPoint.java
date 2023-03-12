f518e9c38b95b63c5df8959e07ec9bf8e24888c5
Akvamarinika <rozespink@mail.ru>
2023-03-12 13:04:50 +0800

    added JwtAuthenticationEntryPoint (for http request unauth)

---
+++ src/main/java/com/akvamarin/friendsappserver/security/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.akvamarin.friendsappserver.security.jwt;
+
+import com.akvamarin.friendsappserver.domain.responseerror.ErrorDescription;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Используется для отправки ответа HTTP.
+ *
+ * Если пользователь неавторизован, но пытается получить доступ к частным ресурсам.
+ **/
+
+@AllArgsConstructor
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper mapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(mapper.writeValueAsString(
+                new ErrorDescription("User not authorized", authException.getMessage())));
+    }
+}
